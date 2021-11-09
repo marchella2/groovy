43825122135216f41bb4f28b772bc0046fe244a9
marchella2 <marchella.putris2@gmail.com>
2021-11-09 14:39:41 +0700

    2. Comment

--- src/com/learngroovy/HelloWorld.groovy
+++ src/com/learngroovy/HelloWorld.groovy
@@ -1,6 +1,13 @@
 package com.learngroovy
 
 class HelloWorld {
+
+    // Single Line Comment
+
+    /*
+     * this is
+     * multi line comment
+    */
     static void main(String[] args) {
         print("Hello World");
     }
